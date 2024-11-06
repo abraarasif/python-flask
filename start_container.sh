5e9cbe4e4491dc1cebd4c9bcb8973889788a87f0
abraarasif <113096239+abraarasif@users.noreply.github.com>
2024-11-06 12:42:27 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull abraarasif123/sample-pythonflask-app
+
 
 # Run the Docker image as a container
-echo
+docker run -it -p 8000:8000 abraarasif123/sample-pythonflask-app
